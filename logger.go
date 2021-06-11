ff259103b2d6e790902f15cf460950ad9d328842
jelena-vlajkov <vlajkovj31@gmail.com>
2021-06-11 20:38:15 +0200

    :sparkles: :bug: Fixed bugs in logger init

--- logger.go
+++ logger.go
@@ -12,24 +12,19 @@ import (
 
 )
 
-type LoggerUseCase interface {
-	InitializeLogger(serviceName string, ctx context.Context)
-	CreateLogDir(filePath string) error
-	CloseLogger()
-	FormatFilePath(file string) string
-}
 
 type Logger struct {
-	logger *logrus.Logger
-	file *os.File
+	Logger *logrus.Logger
+	file   *os.File
 }
 
-func (l *Logger) InitializeLogger(serviceName string, ctx context.Context) {
-	if err := l.CreateLogDir(filepath.FromSlash("../log/logfiles/" + serviceName)); err != nil {
+func InitializeLogger(serviceName string, ctx context.Context) *Logger {
+	logger := &Logger{}
+	if err := CreateLogDir(filepath.FromSlash("../logfiles/" + serviceName)); err != nil {
 		logrus.Fatalf("Failed to create directory for log files | %v\n", err)
 	}
 
-	file := filepath.FromSlash("../log/logfiles/" + serviceName + "/" + serviceName + ".log")
+	file := filepath.FromSlash("../logfiles/" + serviceName + "/" + serviceName + ".log")
 
 	rotatingLogs, err := rotatefilehook.NewRotateFileHook(rotatefilehook.RotateFileConfig{
 		Filename: file,
@@ -38,38 +33,39 @@ func (l *Logger) InitializeLogger(serviceName string, ctx context.Context) {
 		MaxAge: 14,
 		Level: logrus.InfoLevel,
 		Formatter: &logrus.JSONFormatter{
-			TimestampFormat: "01-01-2005 16:21:02",
+			TimestampFormat: "2006-01-02 15:04:05",
 			DataKey: "data",
 			CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
-				return frame.Function, fmt.Sprintf("%s:%d", l.FormatFilePath(frame.File), frame.Line)
+				return frame.Function, fmt.Sprintf("%s:%d", FormatFilePath(frame.File), frame.Line)
 			}},
 	})
 
 	if err != nil {
 		logrus.Fatalf("Failed to initialize rotating hook | %v\n", err)
 	}
-
-	l.logger.AddHook(rotatingLogs)
-	l.logger.SetReportCaller(true)
-	l.logger.SetOutput(os.Stdout)
-	l.logger.SetFormatter(&logrus.TextFormatter{
-		TimestampFormat: "01-01-2005 16:21:02",
+	logger.Logger = logrus.New()
+	logger.Logger.AddHook(rotatingLogs)
+	logger.Logger.SetReportCaller(true)
+	logger.Logger.SetOutput(os.Stdout)
+	logger.Logger.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
 		FullTimestamp: true,
 		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
-			return frame.Function, fmt.Sprintf("%s:%d", l.FormatFilePath(frame.File), frame.Line)
+			return frame.Function, fmt.Sprintf("%s:%d", FormatFilePath(frame.File), frame.Line)
 		}})
-	l.logger.SetLevel(logrus.InfoLevel)
+	logger.Logger.SetLevel(logrus.InfoLevel)
 
+	return &Logger{Logger: logger.Logger, file: logger.file}
 }
 
-func (l *Logger) FormatFilePath(file string) string {
+func FormatFilePath(file string) string {
 	arr := strings.Split(filepath.ToSlash(file), "/")
 	return arr[len(arr) - 1]
 }
 
-func (l *Logger) CreateLogDir(filePath string) error {
+func CreateLogDir(filePath string) error {
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		return os.MkdirAll(filePath, os.ModeDir|0755)
+		return os.MkdirAll(filePath, os.ModeDir|0777)
 	}
 	return nil
 }
@@ -78,6 +74,16 @@ func (l *Logger) CloseLogger() {
 	l.file.Close()
 }
 
-func NewLogger() LoggerUseCase {
-	return &Logger{}
+func (l *Logger) ErrorMessage(message string) {
+	l.Logger.Error(message)
 }
+
+func (l *Logger) InfoMessage(message string) {
+	l.Logger.Info(message)
+}
+
+func (l *Logger) FatalMessage(message string) {
+	l.Logger.Fatal(message)
+}
+
+
